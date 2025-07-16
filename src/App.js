b15f40f4e3f034df5e402beaa0cbc3850d777fe7
Rebekah Reay <becksreay@gmail.com>
2025-07-16 22:57:16 +0100

    First Commit to GitHub
    Moved files from book-share to BookShare so I can use the GitHub project i already set up

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Rebekah Reay Student ID: K2938309
+          TM470 Book Share application
+          First commit to GitHub
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
